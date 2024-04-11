6653c06c168f8eb0de9e7d0f4d3c1c86eb1131fb
Eric Wolf <ericwolf42@gmail.com>
2024-04-11 11:46:48 +0200

    Add some minimal tracing

--- src/main.rs
+++ src/main.rs
@@ -6,7 +6,10 @@ use tower::ServiceBuilder;
 use tower_http::{
     cors::{Any, CorsLayer},
     services::ServeDir,
+    trace::{DefaultMakeSpan, DefaultOnRequest, DefaultOnResponse, TraceLayer},
+    LatencyUnit,
 };
+use tracing::Level;
 
 #[derive(Parser)]
 #[command(version, about, long_about = None)]
@@ -23,13 +26,27 @@ struct CliArgs {
 #[tokio::main]
 async fn main() {
     let cli_args = CliArgs::parse();
+    tracing_subscriber::fmt::init();
+
     let cors = CorsLayer::new()
         .allow_methods(Any)
         .allow_origin(Any)
         .allow_headers(Any)
         .allow_private_network(true);
+    let tracing_layer = TraceLayer::new_for_http()
+        .make_span_with(DefaultMakeSpan::new().include_headers(true))
+        .on_request(DefaultOnRequest::new().level(Level::INFO))
+        .on_response(
+            DefaultOnResponse::new()
+                .level(Level::INFO)
+                .latency_unit(LatencyUnit::Micros)
+                .include_headers(true),
+        );
     let serve_dir = ServeDir::new(cli_args.path);
-    let service = ServiceBuilder::new().layer(cors).service(serve_dir);
+    let service = ServiceBuilder::new()
+        .layer(tracing_layer)
+        .layer(cors)
+        .service(serve_dir);
     let app = Router::new().fallback_service(service);
 
     let listener = tokio::net::TcpListener::bind(cli_args.listens)
